9c1925112131bc38d8cae50445492f17fe1da1b7
VitoriaDutraa <vitoria_dutra@outlook.com.br>
2022-10-15 19:43:18 -0300

    resolucao exercicio

---
+++ resolução-exercicio2.js/Atividade aula 2.js
@@ -0,0 +1,10 @@
+//Nome: Vitória Dutra //
+
+
+
+
+const distancia =  400
+const tempo = 4.0
+const velocidademedia= distancia / tempo
+
+console.log (`A velocidade média é igual á ${distancia / tempo} `);
