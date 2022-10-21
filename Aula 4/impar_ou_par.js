0f7e960fb87720f9b3ab8c7f051fd2eec16ca60d
Lisandra Cruz <lisansouza@gmail.com>
2022-10-20 21:08:27 -0300

    códigos da aula 4

---
+++ Aula 4/impar_ou_par.js
@@ -0,0 +1,7 @@
+/*
+  Crie um programa que verifique se um determinado número é impar ou par!
+  Caso seja ímpar retorne "O número X é ímpar"
+  Caso seja par retorne "O número X é ímpar"
+
+  Dica do sucesso: todo número par é divisível por 2.
+*/
---
+++ Aula 4/logica_condicional.js
@@ -0,0 +1,14 @@
+const a = 2;
+const b = 3;
+const c = 5;
+const d = 9;
+let X;
+
+if (!(d > 5)) {
+  X = (a + b) * d;
+} else {
+  X = (a - b) / c;
+}
+
+console.log(!!(d > 5));
+// console.log("X é igual a :" + X);
